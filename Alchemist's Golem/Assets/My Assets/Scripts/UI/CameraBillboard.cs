828c9ebe20d31af768c9afb2d925bc7deff1a788
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2019-02-20 17:14:47 +0000

    Plot Changes
    Changed the plot prefab
    Add a nutrient list for plot as world UI
    added many UI assets

---
+++ Alchemist's Golem/Assets/My Assets/Scripts/UI/CameraBillboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBillboard : MonoBehaviour
+{
+    public Camera camera;
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, camera.transform.rotation * Vector3.up);
+    }
+}

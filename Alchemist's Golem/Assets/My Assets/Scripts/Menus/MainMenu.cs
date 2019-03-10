88a89950ba385eacd1ea7719ab3172bdbe509750
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2019-03-10 13:58:22 +0000

    Start menu done

---
+++ Alchemist's Golem/Assets/My Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject Hotbar;
+
+    public GameObject Player;
+    public GameObject MenuCamera;
+
+    public GameObject MainMenuUI;
+
+    public void ToggleOn()
+    {
+        Hotbar.SetActive(false);
+        Player.SetActive(false);
+
+        MenuCamera.SetActive(true);
+        MainMenuUI.SetActive(true);
+    }
+
+    public void ToggleOff()
+    {
+        Hotbar.SetActive(true);
+        Player.SetActive(true);
+
+        MenuCamera.SetActive(false);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting");
+        Application.Quit();
+    }
+
+}

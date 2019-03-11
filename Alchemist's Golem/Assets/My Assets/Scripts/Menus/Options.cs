dc6e52ee3461552201dd24f7726a88ecee2bee96
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2019-03-11 14:54:14 +0000

    Started the options menu

---
+++ Alchemist's Golem/Assets/My Assets/Scripts/Menus/Options.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public GameObject OptionsUI;
+    public bool UIon = false;
+
+    public void ToggleOn()
+    {
+        OptionsUI.SetActive(true);
+        UIon = true;
+    }
+
+    public void ToggleOff()
+    {
+        OptionsUI.SetActive(false);
+        UIon = false;
+    }
+}

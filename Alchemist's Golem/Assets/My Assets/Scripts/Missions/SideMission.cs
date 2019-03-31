c84cfe09df33e69260a9761bf2fc54ce32d2ccf0
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2019-03-31 16:05:05 +0100

    Generation of side quests works

---
+++ Alchemist's Golem/Assets/My Assets/Scripts/Missions/SideMission.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideMission : MonoBehaviour
+{
+    public int missionHolderID;
+    public SideMissionManager manager;
+    public ItemDatabase itemDatabase;
+
+    public Text nameDisplay;
+    public Image personDisplay;
+    public Text numWantedDisplay;
+    private int p_numWanted;
+    public Image itemDisplay;
+    private int p_itemID;
+    public Text goldRewardDisplay;
+    private int p_Reward;
+
+    public bool AcceptedQuest = false;
+    public GameObject beforeAcceptGroup;
+    public GameObject HandinGroup;
+
+    public bool CanReject = true;
+    public Button rejectButton;
+    public Button acceptButton;
+
+    public void AcceptMission()
+    {
+        Debug.Log("Accepted Mission");
+    }
+
+    public void RejectMission()
+    {
+        Debug.Log("Rejected Mission");
+    }
+
+    public void CompleteMission()
+    {
+        Debug.Log("Completed Mission");
+    }
+
+    public void PopulateMission(string name, Sprite person, int num, int itemID, int gold)
+    {
+        Debug.Log("Populated Mission: " + missionHolderID);
+
+        p_numWanted = num;
+        p_itemID = itemID;
+        p_Reward = gold;
+
+        nameDisplay.text = name;
+        personDisplay.sprite = person;
+        numWantedDisplay.text = num.ToString();
+        Item temp = itemDatabase.GetItem(itemID);
+        itemDisplay.sprite = temp.itemImage;
+        goldRewardDisplay.text = gold.ToString();
+
+        beforeAcceptGroup.SetActive(true);
+        HandinGroup.SetActive(false);
+    }
+}
---
+++ Alchemist's Golem/Assets/My Assets/Scripts/Missions/SideMissionManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideMissionManager : MonoBehaviour
+{
+    public ItemDatabase itemDatabase;
+
+    public int[] possibleRequestedItems;
+    public string[] possiblePeopleRequesting;
+    public Sprite[] peopleSprites;
+
+    public int requestedItemRewardMulti = 3;
+    public int maxWanted = 20;
+    public int minWanted = 5;
+
+    public SideMission[] sideMissionSlots;
+
+    private void Start()
+    {
+        GenerateForAllQuestSlots();
+    }
+
+    public void GenerateNewQuest(int ID)
+    {
+        //Random person
+        int randPerson = Random.Range(0, possiblePeopleRequesting.Length);
+        string person = possiblePeopleRequesting[randPerson];
+        Sprite personSprite = peopleSprites[randPerson];
+
+        //Random Item
+        int randItem = Random.Range(0, possibleRequestedItems.Length);
+        int item = possibleRequestedItems[randItem];
+
+        //Num of Item
+        int numOfItem = Random.Range(minWanted, maxWanted);
+
+        //Mission Reward
+        Item actualItem = itemDatabase.GetItem(item);
+        int itemSellPrice = actualItem.sellPrice;
+        int basePrice = itemSellPrice * requestedItemRewardMulti;
+        int randomPriceChange = Random.Range(-itemSellPrice, itemSellPrice);
+        int reward = basePrice + randomPriceChange;
+        reward = reward * numOfItem;
+
+        sideMissionSlots[ID].PopulateMission(person, personSprite, numOfItem, item, reward);
+    }
+
+    public void GenerateForAllQuestSlots()
+    {
+        for (int i = 0; i < sideMissionSlots.Length; i++)
+        {
+            GenerateNewQuest(i);
+        }
+    }
+}
